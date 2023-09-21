e1e730ad8dc6b07e10b4c89bb3d5042e0276fc2c
BT_2N2_30 <example@nycstudents.net>
2023-09-21 13:33:41 -0400

    I've added many lines of code and modified some lines of code

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        double totalTip = 0.00;
+
         System.out.println("Welcome to Tip Calculator!");
         System.out.println("I help my users calculate their tips!");
         System.out.println("What's the tip percentage? (Please enter a number from 0 to 100): ");
         double tip = scan.nextDouble();
+        System.out.println("How many people are eating with you?");
+        double totalBill = 0.00;
+        double bill = 0.00;
+
+        System.out.println("Enter -1 to end calculation.");
 
         while (price = -1) {
-            System.out.println("Enter -1 to end calculation.");
-            System.out.println("What is the item name?");
-            System.out.println("What is " + item + "'s price?");
-            double item = scan.nextDouble();
+            if (price != -1) {
+                System.out.println("What is the item's name?");
+                string item = scan.nextString();
+                System.out.println("What is " + item + "'s price?");
+                double itemPrice = scan.nextDouble();
+
+                totalTip = totalTip + (itemPrice * tip);
+            }
         }
 
-        System.out.print("Enter an EVEN integer: ");
-        int number = scan.nextInt();
-        while (number % 2 != 0) {
-            System.out.print("No silly, i said EVEN not odd! Try again: ");
-            number = scan.nextInt();
-        }
-
-        // code below the while loop runs after the loop ends
         System.out.print("Done!");
 
     }
